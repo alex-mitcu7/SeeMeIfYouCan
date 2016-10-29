fe7fa757de1b000f406beaddec2bde01a2a30e0b
Alex Mitcu <alexandru.mitcu@student.manchester.ac.uk>
2016-10-29 21:13:54 +0100

    Update code PixelMatrixGenerator

--- Pixel.java
+++ Pixel.java
@@ -1,10 +1,10 @@
-public class Pixel 
+public class Pixel
 {
   private static int alpha;
   private static int blue;
   private static int green;
-  private static int red;  
-  
+  private static int red;
+
   public Pixel (int alphaGiven, int blueGiven, int greenGiven, int redGiven)
   {
     alpha = alphaGiven;
@@ -12,44 +12,49 @@ public class Pixel
     green = greenGiven;
     red = redGiven;
   }
-  
+
   public int getAlpha ()
   {
     return alpha;
   }
-  
+
     public int getBlue ()
   {
     return blue;
   }
-    
+
   public int getGreen ()
   {
     return green;
   }
-  
+
   public int getRed ()
   {
     return red;
   }
-  
+
   public void setAlpha (int newAlpha)
   {
     alpha = newAlpha;
   }
-  
+
   public void setBlue (int newBlue)
   {
     blue = newBlue;
   }
-  
+
   public void setGreen (int newGreen)
   {
     green = newGreen;
   }
-  
+
   public void setRed (int newRed)
   {
     red = newRed;
-  }  
+  }
+
+  public String toString()
+  {
+    return "Pixel with properties R " + red + " G " + green + " B " + blue;
+  }
 }
--- PixelMatrixGenerator.java
+++ PixelMatrixGenerator.java
@@ -3,13 +3,13 @@ import java.awt.image.DataBufferByte;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-public class PixelMatrixGenerator 
+public class PixelMatrixGenerator
 {
-  public static void main(String[] args) throws IOException 
+  public static void main(String[] args) throws IOException
   {
     // Initialise variable to hold the image.
     BufferedImage image = ImageIO.read(PixelMatrixGenerator.class.getResource(args[0]));
-    
+
     // Array to hold the image pixels.
     final byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
     // Initialise the width and height of the image.
@@ -17,59 +17,44 @@ public class PixelMatrixGenerator
     final int height = image.getHeight();
     // Initialise boolean variable to hold wether or not the image has alpha parameter.
     final boolean hasAlphaChannel = image.getAlphaRaster() != null;
+    int pixelLength = 4;
+    int alpha = 0;
 
     // Initialize the matrix.
     Pixel[][] pixelMatrix = new Pixel[height][width];
-    
+
     // Check for alpha parameter.
-    if (hasAlphaChannel) 
+    if (!hasAlphaChannel)
     {
-      // If the alpha exist, initialize length with 4 (alpha, blue, green, red).
-      final int pixelLength = 4;
-      for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) 
-      {
-        // Initialize colors.
-        int alpha = ((int) pixels[pixel] & 0xff); // alpha
-        int blue = ((int) pixels[pixel + 1] & 0xff); // blue
-        int green = ((int) pixels[pixel + 2] & 0xff); // green
-        int red = ((int) pixels[pixel + 3] & 0xff); // red
-        
-        // Create new Pixel object.
-        pixelMatrix[row][col] = new Pixel(alpha, blue, green, red);
-        System.out.println(pixelMatrix[row][col].getBlue());
-        // Increment col and row.
-        col++;
-        if (col == width) 
-        {
-          col = 0;
-          row++;
-        }
-      }
-    } 
-    else 
+      pixelLength = 3;
+      alpha = 255;
+    } // if
+
+    int row = 0, col = 0;
+    for (int pixel = 0; pixel < pixels.length; pixel += pixelLength)
     {
-      // If the alpha doesn't exist, initialize length with 3 (blue, green, red).
-      final int pixelLength = 3;
-      for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) 
+      // Initialize colors.
+      if(pixelLength == 4)
+        alpha = ((int) pixels[pixel] & 0xff); // alpha
+
+      int blue = ((int) pixels[pixel + 1] & 0xff); // blue
+      int green = ((int) pixels[pixel + 2] & 0xff); // green
+      int red = ((int) pixels[pixel + 3] & 0xff); // red
+
+      // Create new Pixel object.
+      pixelMatrix[row][col] = new Pixel(alpha, blue, green, red);
+      // Increment col and row.
+      col++;
+      if (col == width)
       {
-        // Initialize colors.
-        int alpha = 255; // alpha
-        int blue = ((int) pixels[pixel] & 0xff); // blue
-        int green = ((int) pixels[pixel + 1] & 0xff); // green
-        int red = ((int) pixels[pixel + 2] & 0xff); // red
-        
-        // Create new Pixel object.
-        pixelMatrix[row][col] = new Pixel(alpha, blue, green, red);
-        // Increment col and row.
-        col++;
-        if (col == width) 
-        {
-          col = 0;
-          row++;
-        }
-      }
-    }
-    System.out.println(pixelMatrix[10][15].getBlue());
-    
-  }
-}
+        col = 0;
+        row++;
+      } // if
+    } // for
+
+    for (int i = 0; i < height; i++)
+      for (int j = 0; j < width; j++)
+        System.out.println(pixelMatrix[i][j]);
+
+  } // main
+} // class PixelMatrixGenerator
